d7fe3001897ff8a2882a6045378255167fdb095f
Mathias Hahner <mathias_hahner@hotmail.com>
2023-04-26 03:02:00 -0300

    Criada logica de envio e recebimento de mensagens

--- word-findr/src/main/java/com/wordfindr/commom/Player.java
+++ word-findr/src/main/java/com/wordfindr/commom/Player.java
@@ -1,24 +1,32 @@
-package com.wordfindr.server;
+package com.wordfindr.commom;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.net.Socket;
 import java.util.ArrayList;
 
 @Data
-public class Player {
+public class Player implements Cloneable, Serializable {
 
-    private String name;
     private int score;
+    private String name;
     private int requestedHints;
     private Socket connection;
     private ArrayList<String> guesses;
 
     public Player(String name, Socket connection) {
-        this.name = name;
         this.score = 0;
+        this.name = name;
         this.requestedHints = 0;
         this.connection = connection;
         this.guesses = new ArrayList<>();
+    }
+
+    @Override
+    public Player clone() throws CloneNotSupportedException {
+        Player clone = (Player) super.clone();
+        clone.setConnection(null);
+        return clone;
     }
 }
